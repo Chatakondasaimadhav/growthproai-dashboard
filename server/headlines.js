d909f0665b3d148883cbeb2be11f9c76c37c2b9e
Chatakondasaimadhav <147918415+Chatakondasaimadhav@users.noreply.github.com>
2025-07-06 14:48:24 +0530

    Create headlines.js

---
+++ server/headlines.js
@@ -0,0 +1,7 @@
+module.exports = [
+  "Why [Business] is [Location]'s Sweetest Spot in 2025",
+  "Discover [Business]: The Hidden Gem of [Location]",
+  "How [Business] Became the Talk of [Location]",
+  "[Business] is Redefining Local Flavor in [Location]",
+  "[Location] Can't Get Enough of [Business]'s Charm"
+];
